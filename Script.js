21c912a31fbfac98e2df5b54e391c0613ad91a15
Swaraj-Darekar <swarajdarekar1582004@gmail.com>
2024-06-23 21:25:32 +0530

    making changes and adding photoes in blog section

--- Script.js
+++ Script.js
@@ -68,3 +68,43 @@ const typeEffect = () => {
 }
 
 typeEffect();
+
+
+
+
+// contact us section email sending
+
+
+function sendMail() {
+
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        message: document.getElementById("message").value,
+
+    };
+
+    const serviceID = "service_sy2nw5a";
+
+    const templateID = "template_u4svml6";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(
+            res => {
+                document.getElementById("name").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("phone").value = "";
+                document.getElementById("message").value = "";
+
+                console.log(res);
+
+                alert("your message sent sucessfully..");
+
+
+            }
+        ).catch(err => console.log(err));
+
+
+
+}
